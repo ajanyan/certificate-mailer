7b50f0cddc61082934e821fa5a03a97b26936ba1
ajanyan <31389762+ajanyan@users.noreply.github.com>
2018-01-15 21:04:14 +0530

    Update display.php

--- display.php
+++ display.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+ 
+  <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
+
 	<title>View Certificate</title>
 </head>
 <body>
@@ -10,12 +15,43 @@
       $branch=$_POST["branch"];
       $event = $_POST["event"];
       $email = $_POST["email"];
-      echo"<img src='generate.php?name=$name &sem=$sem&branch=$branch&event=$event' width=1000 height=600";
-      echo "<br>";
-	  require("mail.php");      
 
 	 ?>
+	 <script>
+       var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+
+				
+
+        var xmlhttp1 = new XMLHttpRequest();
+                xmlhttp1.onreadystatechange = function() {
+                  if (this.readyState == 4 && this.status == 200) {
+
+
+
+                    swal({
+                    title: "Success",
+                    text: "Mail Sent",
+                    type: "success",
+                    }, function(){
+                        window.location.href = "index.php";
+                    });
+                    
+                        
+                    }
+                };
+                xmlhttp1.open("GET", "mail.php?name=<?php echo $name ?>&sem=<?php echo $sem ?>&branch=<?php echo $branch ?>&event=<?php echo $event ?>&email=<?php echo $email ?>" , true);
+                xmlhttp1.send();
+
+
+                
+            }
+        };
+        xmlhttp.open("GET", "generate.php?name=<?php echo $name ?>&sem=<?php echo $sem ?>&branch=<?php echo $branch ?>&event=<?php echo $event ?>&email=<?php echo $email ?>" , true);
+        xmlhttp.send();
+
 	 
- 
+ </script>
 </body>
 </html>
